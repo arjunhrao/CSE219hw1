e849b6db4442ed4245200c65025c9f15bbab7391
Arjun Rao <ravirao@10.1.154.66>
2016-06-08 20:40:41 -0400

    Another commit. Haven't gotten much done bc I'm figuring out what's going on, but I've gathered that I can edit the xml file directly and add to the enum in order to get a property. These edits will be made shortly - the only edit from here is that I've created a dialog box (which needs to be changed because I don't have all necessary fields and I need to label them consistently).

--- ToDoListMakerApp/src/tdlm/controller/ToDoListController.java
+++ ToDoListMakerApp/src/tdlm/controller/ToDoListController.java
@@ -3,6 +3,7 @@ package tdlm.controller;
 import java.io.File;
 import java.io.IOException;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
@@ -14,6 +15,16 @@ import javax.imageio.ImageIO;
 import tdlm.data.DataManager;
 import tdlm.gui.Workspace;
 import saf.AppTemplate;
+
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 
 /**
  * This class responds to interactions with todo list editing controls.
@@ -28,14 +39,42 @@ public class ToDoListController {
 	app = initApp;
     }
     
-    public void processAddItem() {	
+    
+    public void processAddItem() {
 	// ENABLE/DISABLE THE PROPER BUTTONS
 	Workspace workspace = (Workspace)app.getWorkspaceComponent();
 	workspace.reloadWorkspace();
         
         //need a popup dialogue box with multiple input fields for all of the todoitem's data
         
+        //creates a stage for the dialog
+        //Stage myStage = new Stage();
         
+        //creates the dialog
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("testing");
+        
+        //adds ok and cancel buttons
+        ButtonType loginButtonType = new ButtonType("OK", ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+        
+        //start creating the boxes/panes for the gui
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20, 150, 10, 10));
+        
+        TextField from = new TextField();
+        from.setPromptText("Category");
+        TextField to = new TextField();
+        to.setPromptText("Description");
+
+        gridPane.add(from, 0, 0);
+        gridPane.add(new Label("Description:"), 1, 0);
+        gridPane.add(to, 2, 0);
+
+        dialog.getDialogPane().setContent(gridPane);
+        dialog.showAndWait();
     }
     
     public void processRemoveItem() {
